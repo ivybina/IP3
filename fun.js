30365c4f34b5363abad0ecbb2c28c345d51c834e
ivybina <ivybina999@gmail.com>
2017-07-27 22:54:53 +0300

    straightening

---
+++ fun.js
@@ -0,0 +1,21 @@
+
+var num = function () {
+  console.log(
+function num() {
+  var num;
+var num = new number();
+if(num==2){
+  return=" Ping";
+}
+else if (num==3) {
+return= "Pong";
+}
+ else if (num ==5) {
+return= "Pingpong";
+}
+else {
+  return 0;
+}
+
+}
+)}
