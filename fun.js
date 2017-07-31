f2f730fd73803d560027aa86e9f2447e4d63c67e
ivybina <ivybina999@gmail.com>
2017-07-31 21:05:33 +0300

    trying to use js

--- fun.js
+++ fun.js
@@ -1,37 +1,16 @@
-//
-// var num = function () {
-//   console.log(
-// function num() {
-//   var num =parseInt(document.getElementsById('number').value);
-// if(num==2){
-//   return=" Ping";
-// }
-// else if (num==3) {
-// return= "Pong";
-// }
-//  else if (num ==5) {
-// return= "Pingpong";
-// }
-// else {
-//   return 0;
-// }
-//
-// }
-// var ans;
-// )}
-// function makeArray(count, content) {
-//    var result = [];
-//    if(typeof(content) == "function") {
-//       for(var i=0; i<count; i++) {
-//          result.push(content(i));
-//       }
-//    } else {
-//       for(var i=0; i<count; i++) {
-//          result.push(content);
-//       }
-//    }
-//    return result;
-// }
-//
-// alert(makeArray(8, 1));
-// alert(makeArray(8, function(i) { return i*3; }));
+$(document).ready(function (){
+function countInput() {
+
+  var select= document.getElementsById('user_input');
+
+   for (var i = 0; i <= select; i++){
+
+   user_input += i + "<br>";
+       }
+
+  }
+}
+
+});
+document.getElementById("demo").innerHTML = user_input;
+    // document.getElementById("user_input").innerHTML = select;
