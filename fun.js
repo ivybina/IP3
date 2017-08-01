a1f6bca982005a736e7df11690e85e13a1dfec4a
ivybina <ivybina999@gmail.com>
2017-08-01 21:08:59 +0300

    debugging

--- fun.js
+++ fun.js
@@ -1,3 +1,33 @@
+var pingpongLoop = function(userInput) {
+
+
+var resultOfArray = [];
+for(var i = 1; i <= userInput; i += 1) {
+
+  if (((i % 3) === 0) && ((i % 5) === 0)) {
+    return resultOfArray.push("pingpong");
+  } else if ((i % 3) === 0) {
+    return resultOfArray.push("Ping");
+  }  else if ((i % 5) === 0){
+    return resultOfArray.push("Pong");
+  } else {
+    return resultOfArray.push(i);
+   }
+}
+return resultOfArray;
+}
+
+
+$(document).ready (function() {
+   $("#submit").click(function(event){
+     event.preventDefault();
+     var userInput = parseInt($("#user_input").val());
+     var resultOfArray = pingpongLoop(userInput);
+     $("#display").text(resultOfArray);
+    });
+  });
+
+
 
 
 
