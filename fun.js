631749019aea53021b2b3f7ea5d2267f1c282c1a
ivybina <ivybina999@gmail.com>
2017-08-01 17:25:38 +0300

    displaying arlets

--- fun.js
+++ fun.js
@@ -1,16 +1,42 @@
-$(document).ready(function (){
-function countInput() {
 
-  var select= document.getElementsById('user_input');
 
-   for (var i = 0; i <= select; i++){
 
-   user_input += i + "<br>";
-       }
 
-  }
-}
 
-});
-document.getElementById("demo").innerHTML = user_input;
-    // document.getElementById("user_input").innerHTML = select;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function showInput(){
+//
+//   var select= document.getElementsById('submit');
+//  //  var start=0;
+//  //  while (start<=select) {
+//  //   document.write ("start + <br/>")
+//  //   start=start + 1;
+//  // }
+//
+//    for (var i = 0; i <= select; i++){
+//
+//    user_input += i + "<br>";
+//        }
+//
+// });
+// document.getElementById("submit").innerHTML = submit;
+//     // document.getElementById("user_input").innerHTML = select;
